32145dc229c840b916a43893d91e85e37759031d
Stitchou <la_muerte_97216@hotmail.com>
2016-03-02 17:09:45 +0100

    retirer bootstrap mise en place css php normal

--- BlackJack/Templates/index.dwt.php
+++ BlackJack/Templates/index.dwt.php
@@ -1,14 +1,6 @@
 <!doctype html>
 <html>
 <head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <meta charset="utf-8">
 <!-- TemplateBeginEditable name="doctitle" -->
 <title>BlackJack</title>
@@ -20,26 +12,13 @@
          
 
 <body>
-<div class="container">
-      <div class="col-md-10 mainImg">
-      	<div class="row">
-          <div class="col-md-4"></div>
-          <div class="col-md-4">sfyguhjk</div>
-          <div class="col-md-4"></div>
-        </div>
-        <div class="row">
-          <div class="col-md-4"></div>
-          <div class="col-md-4">fdgjlk</div>
-          <div class="col-md-4"></div>
-        </div>
-        <div class="row">
-          <div class="col-md-4">try</div>
-          <div class="col-md-4">sqsdfgh</div>
-          <div class="col-md-4">sdfgh</div>
-        </div>
-      </div>
-      <div class="col-md-2">.col-md-4 .col-md-offset-4</div>
-
+<div class="main" id="main">
+  <div class="table" id="table">
+    
+    </div>
+  
+  <div class="topScore" id="topScore">Placez ici le contenu de  class "topScore" id "top"
+    </div>
 </div>
 </body>
 </html>
