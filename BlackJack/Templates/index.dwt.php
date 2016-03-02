ec84d8355ed96152c0bcd7dc2913e55196832059
Stitchou <la_muerte_97216@hotmail.com>
2016-03-02 18:41:52 +0100

    mise en place + positionnement des DIVS ( label pour feedback user )

--- BlackJack/Templates/index.dwt.php
+++ BlackJack/Templates/index.dwt.php
@@ -11,18 +11,22 @@
 </head>
          
 
-<body>
-<div class="main" id="main">
-  <div class="table" id="table">
-    <div class="bankHand" id="bankHand">Placez ici le contenu de  class "bankHand" id "bankHand"</div>
-    <div class="playerHand" id="playerHand">Placez ici le contenu de  class "bankHand" id "bankHand"</div>
-    <div class="balance" id="balance">Placez ici le contenu de  class "bankHand" id "bankHand"</div>
-    <div class="bet" id="bet">Placez ici le contenu de  class "bankHand" id "bankHand"</div>
-    </div> 
-    
-
-     <div class="topScore" id="topScore">Placez ici le contenu de  class "topScore" id "top"
-  </div>
-</div>
-</body>
+	<body>
+		<div class="main" id="main">
+			<div class="table" id="table">
+                <div class="bankHand" id="bankHand">class "bankHand" id "bankHand"</div>
+                <div class="playerHand" id="playerHand">Pclass "playerHand" id "playerHand"</div>
+                <div class="balance" id="balance">BALANCE</div>
+                <div class="bet" id="bet">BET</div>
+			</div> 
+ 			<div class="topScore" id="topScore">
+            	<div class="score">
+	 				Placez ici le contenu de  class "topScore" id "top"
+                 </div>
+                <div class="help">
+                	AIDE
+                </div>
+  			</div>
+		</div>
+	</body>
 </html>
