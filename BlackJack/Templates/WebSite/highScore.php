049a50f1511cfb6dcec266fd99100c8ccb02d68b
Stitchou <la_muerte_97216@hotmail.com>
2016-03-03 10:27:02 +0100

    ajout top score

---
+++ BlackJack/Templates/WebSite/highScore.php
@@ -0,0 +1,22 @@
+<?php
+try
+{
+	//A changer pr la soutenance
+	//$bdd = new PDO('mysql:host=localhost;dbname=blackjack;charset=utf8', 'Black', 'BlackJack');
+	$bdd = new PDO('mysql:host=localhost;dbname=blackjack;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+		die('Erreur : ' . $e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT * FROM score order by score DESC limit 10');
+
+
+while ($donnees = $reponse->fetch())
+{
+	echo $donnees['nom'] . ' --> '.$donnees['score'].'<br />';
+}
+
+$reponse->closeCursor();
+?>
