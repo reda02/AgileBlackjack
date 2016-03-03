5ef039d8a2f0eb36eca4139ff0849f1c8f974919
Mickaël Lafages <mickx971.l@gmail.com>
2016-03-03 11:18:16 +0100

    mise.php

---
+++ BlackJack/Templates/WebSite/mise.php
@@ -0,0 +1,2 @@
+<input id="inputBeginMise" data-slider-ticks="[0,100,200, 300]" data-slider-id='inputBeginMiseSlider' type="text" data-slider-min="0" data-slider-max="300" data-slider-step="10" data-slider-ticks-labels='["$0",  "$100","$200","$300"]' data-slider-value="0"/>
+<button class="btn btn-info">MISER</button>
